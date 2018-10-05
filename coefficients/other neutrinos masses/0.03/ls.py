767246cf65b21436338830a9b8700570b8dfe5fc
Valcin <valcin.david@hotmail.fr>
2018-10-05 18:22:23 +0200

    new ls coeff

--- coefficients/other neutrinos masses/0.03/ls.py
+++ coefficients/other neutrinos masses/0.03/ls.py
@@ -68,20 +68,56 @@ for j in xrange(0,len(z)):
 	#----------------------------------------------------------------
 
 	#~ #compute tinker stuff
-	limM = [5e11,1e12,3e12,1e13, 3.2e15]
-	limM = [4.5e11,1e12,3e12,1e13, 3.5e15]
-	loglim = [ 11.653, 12., 12.477, 13., 15.544]
-	cname = 'Pk_cb_z='+str(z[j])+'00.txt'
-
+	limM = [4.2e11,1e12,3e12,1e13, 5e15]
+	loglim = [ 11.623, 12., 12.477, 13., 15.698]
+	cname = 'Pk_cc_z='+str(z[j])+'00.txt'
+	camb = np.loadtxt(cname)
+	kcamb = camb[:,0]
+	Pcamb = camb[:,1]
+	
+	massf = np. loadtxt('/home/david/codes/Paco/data2/0.15eV/hmf_z='+str(z[j])+'.txt')
+	m_middle = massf[:,10]
+	dm = massf[:,11]
 
 	#### get tinker and crocce hmf
-	Tb1 = halo_bias('Tinker', z[j], limM[0],limM[4], cname,Omega_c, Omega_b, do_DM=True )
-	Tb2 = halo_bias('Tinker', z[j], limM[1],limM[4], cname,Omega_c, Omega_b, do_DM=True )
-	Tb3 = halo_bias('Tinker', z[j], limM[2],limM[4], cname,Omega_c, Omega_b, do_DM=True )
-	Tb4 = halo_bias('Tinker', z[j], limM[3],limM[4], cname,Omega_c, Omega_b, do_DM=True )
-
-	print Tb1, Tb2, Tb3, Tb4
-	with open('LS_z='+str(z[j])+'_.txt', 'w+') as fid_file:
-		fid_file.write('%.8g %.8g %.8g %.8g\n' % (Tb1,Tb2, Tb3, Tb4))
+	bt=np.empty(len(m_middle),dtype=np.float64)
+	for i in range(len(m_middle)):
+			bt[i]=bias(kcamb,Pcamb,Omega_m,m_middle[i],'Tinker')
+			
+			
+	dndM=MFL.Tinker_mass_function(kcamb,Pcamb,Omega_m,z[j],limM[0],limM[4],len(m_middle),Masses=m_middle)[1]
+	with open('/home/david/codes/Paco/data2/other neutrinos masses/'+str(nv)+'/thmf_z='+str(z[j])+'.txt', 'w+') as fid_file:
+		for m in xrange(0, len(m_middle)):
+			fid_file.write('%.8g\n' % (dndM[m]))
 	fid_file.close()
+	dndMbis=MFL.Crocce_mass_function(kcamb,Pcamb,Omega_m,z[j],limM[0],limM[4],len(m_middle),Masses=m_middle)[1]
+	with open('/home/david/codes/Paco/data2/other neutrinos masses/'+str(nv)+'/chmf_z='+str(z[j])+'.txt', 'w+') as fid_file:
+		for m in xrange(0, len(m_middle)):
+			fid_file.write('%.8g\n' % (dndMbis[m]))
+	fid_file.close()
+		
+		
+	dndM = np.loadtxt('/home/david/codes/Paco/data2/other neutrinos masses/'+str(nv)+'/thmf_z='+str(z[j])+'.txt')
+	dndMbis = np.loadtxt('/home/david/codes/Paco/data2/other neutrinos masses/'+str(nv)+'/chmf_z='+str(z[j])+'.txt')
+	bin1 = np.where(m_middle > 5e11 )[0]
+	bin2 = np.where(m_middle > 1e12 )[0]
+	bin3 = np.where(m_middle > 3e12 )[0]
+	bin4 = np.where(m_middle > 1e13 )[0]	
 	
+	### CROCCE
+	Bias_eff_t1=np.sum(dndMbis[bin1]*dm[bin1]*bt[bin1])/np.sum(dm[bin1]*dndMbis[bin1])
+	Bias_eff_t2=np.sum(dndMbis[bin2]*dm[bin2]*bt[bin2])/np.sum(dm[bin2]*dndMbis[bin2])
+	Bias_eff_t3=np.sum(dndMbis[bin3]*dm[bin3]*bt[bin3])/np.sum(dm[bin3]*dndMbis[bin3])
+	Bias_eff_t4=np.sum(dndMbis[bin4]*dm[bin4]*bt[bin4])/np.sum(dm[bin4]*dndMbis[bin4])
+	### TINKER
+	bias_eff_t1=np.sum(dndM[bin1]*dm[bin1]*bt[bin1])/np.sum(dm[bin1]*dndM[bin1])
+	bias_eff_t2=np.sum(dndM[bin2]*dm[bin2]*bt[bin2])/np.sum(dm[bin2]*dndM[bin2])
+	bias_eff_t3=np.sum(dndM[bin3]*dm[bin3]*bt[bin3])/np.sum(dm[bin3]*dndM[bin3])
+	bias_eff_t4=np.sum(dndM[bin4]*dm[bin4]*bt[bin4])/np.sum(dm[bin4]*dndM[bin4])
+
+	with open('with_Crocce_HMF/LS_z='+str(z[j])+'_.txt', 'w+') as fid_file:
+		fid_file.write('%.8g %.8g %.8g %.8g\n' % (Bias_eff_t1,Bias_eff_t2, Bias_eff_t3, Bias_eff_t4))
+	fid_file.close()
+	with open('with_Tinker_HMF/LS_z='+str(z[j])+'_.txt', 'w+') as fid_file:
+		fid_file.write('%.8g %.8g %.8g %.8g\n' % (bias_eff_t1,bias_eff_t2, bias_eff_t3, bias_eff_t4))
+	fid_file.close()
