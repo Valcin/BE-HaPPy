435d58cbcb0892d062986d02b0c3c4f34a770be6
Valcin <valcin.david@hotmail.fr>
2019-03-05 15:43:35 +0100

    changes

--- __init__.py
+++ __init__.py
@@ -44,8 +44,6 @@ class BE_HaPPy(Likelihood):
 		self.Psimu = Pmono1
 		self.err = errPr1
 		self.noise = noise1
-		print self.noise
-		kill
 		
 		#~ THINK OF HST PRIORS
 		# Else the file will be created in the loglkl() function.
@@ -226,7 +224,7 @@ class BE_HaPPy(Likelihood):
 		# compute ps in redshift space
 		Pred = np.zeros((len(kbound)))
 		Pred = PhhDD*coeffA  + 2/3.*fz*PhhDT*coeffB + 1/5.*fz**2*Pmod_tt*coeffC + 1/3.*AB2*coeffB \
-		+ 1/5.*AB4*coeffC + 1/7.*AB6*coeffD + 1/9.*AB8*coeffE + A_shot 
+		+ 1/5.*AB4*coeffC + 1/7.*AB6*coeffD + 1/9.*AB8*coeffE + A_shot * self.noise
 		
 		
 		### interpolate data on kbound
