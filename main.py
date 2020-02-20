13519be02a7993eaafaa997a07d1f64315e13ae0
Valcin <valcin.david@hotmail.fr>
2020-02-20 17:35:39 +0100

    updates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from coeff import bcoeff
 from rescaling import rescaling
 from power_spec import red_ps
 
-def ps_calc(kcase, Mnu, mbin, rsd, bmodel, kbound, z, fz, Dz, fog, sigma_v, A_shot):
+def ps_calc(kcase, Mnu, mbin, rsd, bmodel, kbound, z, fz, Dz, fog, A_shot):
 	print 'Total neutrino mass is ' + str(Mnu)+'eV'
 	print 'you chose the mass bin M' + str(mbin +1)
 	if rsd == 1:
@@ -61,6 +61,6 @@ def ps_calc(kcase, Mnu, mbin, rsd, bmodel, kbound, z, fz, Dz, fog, sigma_v, A_sh
 	### compute the redshift power spectrum
 	
 	Pred = red_ps(mbin, bmodel, kbound, z, fz, Dz, b1, b2, b3, b4, A, B, C, D, E, F, G, H, Pmod_dd,
-	Pmod_dt, Pmod_tt, alpha, fog, sigma_v, A_shot, rsd)
+	Pmod_dt, Pmod_tt, alpha, fog, A_shot, rsd, red, kcase)
 
 	return
